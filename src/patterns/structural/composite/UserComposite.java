9df2842c3cd018df5d388319266d22a0bad3c7b6
Sevastian Bajenov <sevabaj@gmail.com>
2023-09-28 11:09:32 +0300

    Implement composite pattern.

---
+++ src/patterns/structural/composite/UserComposite.java
@@ -0,0 +1,44 @@
+package src.patterns.structural.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import src.app.client.User;
+
+public class UserComposite extends User {
+    
+    private List<User> users = new ArrayList<>();
+
+    public UserComposite(String name, boolean subscriptionStatus) {
+        super(name, subscriptionStatus);
+    }
+
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+
+    public void removeUser(User user) {
+        this.users.remove(user);
+    }
+
+    public void watchMovie(String movieName, int episodeTime) {
+        System.out.println(this.getClass().getName() + " is watching movie");
+        for (User user : this.users) {
+            user.watchMovie(movieName, episodeTime);
+        }
+    }
+
+    public void downloadMovie(String movieName) {
+        System.out.println(this.getClass().getName() + " is downloading movie");
+        for (User user : this.users) {
+            user.downloadMovie(movieName);
+        }
+    }
+
+    public void cancelSubscription() {
+        System.out.println(this.getClass().getName() + " is trying to cancel subscription");
+        for (User user : this.users) {
+            user.cancelSubscription();
+        }
+    }
+}
